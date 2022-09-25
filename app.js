c104d55f75a23d5ea8fbb18fd217dbc8fdeaf7ef
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2022-09-25 16:02:53 -0700

    blocked out the js that wasnt work for me today

--- app.js
+++ app.js
@@ -16,26 +16,26 @@ if(time <= 11){
     message = "That hour does not exist!";
 }
 
-let = prompt("Do you eat fresh?");
+let any = prompt("Do you eat fresh?");
 console.log('Keep it up,' + username);
 
-let yes = prompt("You are awesome, keep it up");
+// let yes = prompt("You are awesome, keep it up");
 let words;
 
-let yes = "Stay Fresh!";             
-let no = "'Fresh eats miss you too!'"; 
+// let yes = "Stay Fresh!";             
+// let no = "'Fresh eats miss you too!'"; 
 
-if(yes = {
-    words = "You are awesome, keep it up!");
-if(no = {
-    words = "You are awesome and fresh veggies miss you.";
+// if(yes = {
+    // words = "You are awesome, keep it up!");
+// if(no = {
+    // words = "You are awesome and fresh veggies miss you.";
 
- yes = prompt ('You are awesome keep it up');
-document.write('You are awesome. Keep it up.' + username );
+//  yes = prompt ('You are awesome keep it up');
+// document.write('You are awesome. Keep it up.' + username );
 
 
 
 
 
 // look for the "<h2><script src="app.js"></script></h2>" on line 20 in the HTML, this is where the message will write too!
-document.write('Hello ' + username + '! ' + message);
+// document.write('Hello ' + username + '! ' + message);
